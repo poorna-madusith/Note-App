3da212530fbe63e45984beec4c18f93b097a0803
Poorna Madusith <157389469+poorna-madusith@users.noreply.github.com>
2025-03-17 17:34:26 +0530

    Created get all notes api

--- backend/index.js
+++ backend/index.js
@@ -195,6 +195,25 @@ app.put("/edit-note/:noteId",authenticateToken,async(req,res)=>{
 })
 
 
+//get all notes
+app.get("/get-all-notes",authenticateToken,async(req,res)=>{
+
+    const {user} = req.user;
+
+    try{
+        const notes = await Note.find({userId: user._id}).sort({isPinned:-1});
+
+        return res.json({error:false,notes,message:"All notes recived successfully"})
+
+
+
+    }catch(error){
+        return res.status(500).json({error:true, message:"Internal server error"});
+    }
+
+    })
+
+
 
 app.listen(8000);
 
